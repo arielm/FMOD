3685cb0325b694f86f9ac69e342d0d3db2c86e96
Ariel Malka <ariel@chronotext.org>
2013-07-11 18:20:28 +0300

    IMPORTING ORIGINAL STUFF

---
+++ Android.mk
@@ -0,0 +1,8 @@
+LOCAL_PATH := $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := fmodex
+LOCAL_SRC_FILES := lib/android/$(TARGET_ARCH_ABI)/libfmodex.so
+LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/include
+
+include $(PREBUILT_SHARED_LIBRARY)
