e3f345f3eba02e49f5fc318bd5cadded91232c17
Ariel Malka <ariel@chronotext.org>
2013-10-03 17:28:44 +0300

    ADAPTING TO "NEW ANDROID FORMAT"

--- Android.mk
+++
@@ -1,8 +0,0 @@
-LOCAL_PATH := $(call my-dir)
-include $(CLEAR_VARS)
-
-LOCAL_MODULE := fmodex
-LOCAL_SRC_FILES := lib/android/$(TARGET_ARCH_ABI)/libfmodex.so
-LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/include
-
-include $(PREBUILT_SHARED_LIBRARY)
